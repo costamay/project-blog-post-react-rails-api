e71ee6c6a9cff6010615367add51c38ba8e76f06
costamay <titusouko@gmail.com>
2022-08-23 18:26:10 +0300

    Add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+  before_action :authorize
+
+  private
+
+  def render_not_found
+      @model_name = controller_name.classify
+      render json: {error: "#{@model_name} not found"}, status: :not_found
+  end
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def render_invalid(e)
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
 end
