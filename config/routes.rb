c2614a6f4296420804ecdc136d77388df436e4ba
costamay <titusouko@gmail.com>
2022-08-23 20:33:17 +0300

    Add comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post "login/", to: "sessions#create"
   delete "logout/", to: "sessions#destroy"
 
-  resources :comments
+  resources :comments, only: [:create]
   resources :categories, only: [:index, :create]
   resources :posts
   resources :profiles
